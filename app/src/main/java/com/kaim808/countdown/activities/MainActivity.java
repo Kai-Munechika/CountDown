ec69f5c1100c8eef1e267e12727ab30a7cf04f02
Kai Munechika <kaim808@gmail.com>
2019-02-23 00:20:07 -1000

    removed pull down to refresh recycler view

--- app/src/main/java/com/kaim808/countdown/activities/MainActivity.java
+++ app/src/main/java/com/kaim808/countdown/activities/MainActivity.java
@@ -13,7 +13,6 @@ import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -35,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     public static String COUNTER_BROADCAST = "COUNTER_BROADCAST";
     public static String ITEM_ID = "ITEM_ID";
 
-    private SwipeRefreshLayout swipeContainer;
     List<Item> items;
     RecyclerView recyclerView;
     ItemAdapter adapter;
@@ -60,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initSwipeContainer();
         initRecyclerView();
         initListDividers();
         initFab();
@@ -90,22 +87,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         broadcastManager.unregisterReceiver(broadcastReceiver);
-    }
-
-    private void initSwipeContainer() {
-        swipeContainer = findViewById(R.id.swipeContainer);
-
-        // Setup refresh listener which triggers new data loading
-        swipeContainer.setOnRefreshListener(() -> {
-            adapter.refresh();
-            swipeContainer.setRefreshing(false);
-        });
-        // Configure the refreshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-
     }
 
     private void initRecyclerView() {
